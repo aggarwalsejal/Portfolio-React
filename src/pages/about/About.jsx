ddb799ac799a2d3735c279601e431715cbd1294b
sejal175 <sejalaggarwal0@gmail.com>
2023-07-25 18:05:21 +0530

    About Personal Data & Stats

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
-
+import Info from '../../components/Info';
+import Stats from '../../components/Stats';
+import { FaDownload } from 'react-icons/fa';
+import CV from "../../assets/Sejal_Aggarwal_Resume_04-06-2023-01-59-28.pdf"
+import './about.css'
 const About = () => {
   return (
     <main className='section container'>
       <section className='about'>
         <h2 className='section__title'>
-          About<span>Me</span>
+          About <span>Me</span>
         </h2>
+        <div className='about__container grid'>
+          <div className='about__info'>
+            <h3 className='section__subtitle'>
+              Personal Information
+            </h3>
+            <ul className='info__list grid'><Info /></ul>
+            <a href='' className='button'>
+              Download CV{' '}
+              <span className='button__icon'>
+                <FaDownload />
+              </span>
+            </a>
+          </div>
+          <div className="stats grid">
+            <Stats />
+          </div>
+        </div>
       </section>
     </main>
   )
