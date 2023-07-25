41865dc9227ad379764a48f9264adee33f076861
sejal175 <sejalaggarwal0@gmail.com>
2023-07-25 15:18:12 +0530

    About page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <main className='section container'>
+      <section className='about'>
+        <h2 className='section__title'>
+          About<span>Me</span>
+        </h2>
+      </section>
+    </main>
   )
 }
 
